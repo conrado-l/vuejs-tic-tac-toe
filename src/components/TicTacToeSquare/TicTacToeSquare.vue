e11b361bdf92d019b3d81b60c77a12ab1014c945
Conrado Levy <conrado.lk@gmail.com>
2022-09-10 19:59:14 -0300

    feat: added Tic Tac Toe basic view and logic

---
+++ src/components/TicTacToeSquare/TicTacToeSquare.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+  player: String,
+  index: Number,
+});
+</script>
+
+<template>
+  <div class="tic-tac-toe-square">
+    <div :class="classes">{{ player }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TicTacToeSquare",
+
+  computed: {
+    classes() {
+      if (this.player === "X") {
+        return "cross";
+      } else if (this.player === "0") {
+        return "circle";
+      }
+      return "default";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tic-tac-toe-square {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 3.5em;
+  height: 3.5em;
+  border: 1px solid black;
+  cursor: pointer;
+}
+.cross {
+  color: red;
+}
+
+.circle {
+  color: green;
+}
+</style>
--- src/views/TicTacToe.vue
+++ src/views/TicTacToe.vue
@@ -1,11 +1,77 @@
 <template>
-  <div> A Tic Tac Toe </div>
+  <div class="container">
+    <header>Tic Tac Toe</header>
+    <div>Current player: {{ currentPlayer }}</div>
+    <div class="tic-tac-toe-squares">
+      <TicTacToeSquare
+        v-for="(square, index) in ticTacToe"
+        :va="square"
+        :key="index"
+        :index="index"
+        @click="onSquareClicked(index)"
+      ></TicTacToeSquare>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import TicTacToeSquare from "@/components/TicTacToeSquare";
+
 export default {
   name: "TicTacToe",
+
+  components: { TicTacToeSquare },
+
+  data() {
+    return {
+      ticTacToe: [
+        "0", "0", "0",
+        "0", "0", "0",
+        "0", "0", "0"
+      ],
+      currentPlayer: "X",
+      isGameFinished: false,
+    };
+  },
+
+  methods: {
+    onSquareClicked(index: number) {
+      if (this.isGameFinished || !this.isSquareFree(index)) {
+        return;
+      }
+
+      this.ticTacToe[index] = this.currentPlayer;
+      this.checkWinner();
+
+      if (!this.isGameFinished) {
+        this.switchPlayer();
+      }
+    },
+
+    isSquareFree(squareIndex: number): boolean {
+      return this.ticTacToe[squareIndex] === "0";
+    },
+
+    switchPlayer() {
+      this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+    },
+
+    checkWinner() {
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tic-tac-toe-squares {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+}
+</style>
